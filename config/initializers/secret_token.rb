b4da6fac0d42842a979c19ca4ec8e4b34189a504
Megatv <konstantin.k.ed@gmail.com>
2012-07-15 17:49:05 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mig::Application.config.secret_token = '2ca042f99bb018f2ee8cb8bd9979773aefeeb94011b09f86512a3d05fbe480c45a31c45de3ab35e68aef8b78c8396256d9f546328f94bf85f9bd185102c8adba'
